ff3de863d65d811a4985ba8bb9557cac6b6cd6f6
YusukeHidaka <ysk91.akg@gmail.com>
2017-10-03 12:00:00 +0900

    Change style

--- src/components/CategoryForm.js
+++ src/components/CategoryForm.js
@@ -4,13 +4,15 @@ import Select from 'react-select';
 
 const MAIN_CATEGORIES = require('../data/main_categories');
 const SUB_CATEGORIES = require('../data/sub_categories');
+const THIRD_CATEGORIES = require('../data/third_categories');
 
 class CategoryForm extends Component {
   constructor (props){
     super(props);
     this.state = {
       mainSelectedOption: null,
-      subSelectedOption: null
+      subSelectedOption: null,
+      thirdSelectedOption: null
     }
   }
   onChangeMain = (mainSelectedOption) => {
@@ -21,25 +23,54 @@ class CategoryForm extends Component {
     this.setState({ subSelectedOption });
   }
 
+  onChangeThird = (thirdSelectedOption) => {
+    this.setState({ thirdSelectedOption });
+  }
+
   render() {
+    // Set value to selector
     let maincatValue = _.get(this.state, 'mainSelectedOption.value');
     let subcatValue = _.get(this.state, 'subSelectedOption.value');
+    let thirdcatValue = _.get(this.state, 'thirdSelectedOption.value');
+
+    // Set options for selector
     const mainOptions = MAIN_CATEGORIES;
     let subOptions = SUB_CATEGORIES[maincatValue];
+    let thirdOptions = THIRD_CATEGORIES[subcatValue];
 
+    // Define abled args
+    let subAbled = true;
+    let thirdAbled = true;
+
+    // Handle placeholder depending on parent category
     let subcatPlaceholder='Choose main category first';
     if(maincatValue){
       subcatPlaceholder = 'Select ...';
+      subAbled = false;
     }
 
+    let thirdcatPlaceholder='Choose sub category';
+    if(subcatValue){
+      if (thirdOptions) {
+        thirdcatPlaceholder = 'Select ...';
+        thirdAbled = false;
+      } else {
+        thirdcatPlaceholder = "This subcategory doesn't have 3rd category";
+      }
+    }
+
+    // Render urls according to selected options
     let url = '';
     if(!maincatValue){
-      url = 'Choose main category / Choose sub category /';
+      url = '{ Choose main category } / { Choose sub category } / { Choose third category } /';
     } else if (!subcatValue) {
-      url = maincatValue + ' / Choose sub category /';
+      url = maincatValue + ' / { Choose sub category } / { Choose third category } /';
+    } else if (thirdcatValue) {
+        url = maincatValue + ' / ' + subcatValue + ' / ' + thirdcatValue + ' /'
     } else {
       url = maincatValue + ' / ' + subcatValue + ' /'
     }
+
 
     return (
       <div className="category-form">
@@ -47,22 +78,31 @@ class CategoryForm extends Component {
         <div className="category-selector">
           <h4>Main category</h4>
           <Select
-            name="form-field-name"
+            name="main-cat-selector"
             value={_.get(this.state, 'mainSelectedOption.value', '')}
             onChange={this.onChangeMain}
             options={mainOptions}
           />
           <h4>Sub category</h4>
           <Select
-            name="form-field-name"
+            name="sub-cat-selector"
             value={_.get(this.state, 'subSelectedOption.value', '')}
             onChange={this.onChangeSub}
             options={subOptions}
             placeholder={subcatPlaceholder}
+            disabled={subAbled}
           />
-          <h4>3rd category(if it exsists)</h4>
+          <h4>third category(if it exsists)</h4>
+          <Select
+            name="third-cat-selector"
+            value={_.get(this.state, 'thirdSelectedOption.value', '')}
+            onChange={this.onChangeThird}
+            options={thirdOptions}
+            placeholder={thirdcatPlaceholder}
+            disabled={thirdAbled}
+          />
         </div>
-        <h3>URL is gonna be: <span>/ article / {url} article_title</span></h3>
+        <p>URL : <span>https://www.estopolis.com / article / {url} article_title</span></p>
       </div>
     );
   }

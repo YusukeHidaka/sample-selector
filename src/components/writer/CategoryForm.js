fe818071a627df955714a195ba4b6cfa2d4f0cb8
YusukeHidaka <ysk91.akg@gmail.com>
2018-01-17 15:55:54 +0700

    Delete useless form about third cat

--- src/components/writer/CategoryForm.js
+++ src/components/writer/CategoryForm.js
@@ -6,7 +6,6 @@ import MainCategoryForm from './MainCategoryForm.js';
 
 const MAIN_CATEGORIES = require('../../data/main_categories');
 const SUB_CATEGORIES = require('../../data/sub_categories');
-const THIRD_CATEGORIES = require('../../data/third_categories');
 
 class CategoryForm extends Component {
   constructor (props){
@@ -14,7 +13,6 @@ class CategoryForm extends Component {
     this.state = {
       mainSelectedOption: null,
       subSelectedOption: null,
-      thirdSelectedOption: null
     }
   }
 
@@ -28,31 +26,23 @@ class CategoryForm extends Component {
       this.setState({ mainSelectedOption: MAIN_CATEGORIES[event.currentTarget.dataset.id] });
     }
     this.setState({ subSelectedOption: null });
-    this.setState({ thirdSelectedOption: null });
   }
 
   onChangeSub = (subSelectedOption) => {
     this.setState({ subSelectedOption });
-    this.setState({ thirdSelectedOption: null });
   }
 
-  onChangeThird = (thirdSelectedOption) => {
-    this.setState({ thirdSelectedOption });
-  }
 
   render() {
     // Set value to selector
     let maincatValue = _.get(this.state, 'mainSelectedOption.value');
     let subcatValue = _.get(this.state, 'subSelectedOption.value');
-    let thirdcatValue = _.get(this.state, 'thirdSelectedOption.value');
 
     // Set options for sub, third selector
     let subOptions = SUB_CATEGORIES[maincatValue];
-    let thirdOptions = THIRD_CATEGORIES[subcatValue];
 
     // Define abled args
     let subDisabled = true;
-    let thirdDisabled = true;
 
     // Handle placeholder depending on parent category
     let subcatPlaceholder='Choose main category first';
@@ -61,32 +51,20 @@ class CategoryForm extends Component {
       subDisabled = false;
     }
 
-    let thirdcatPlaceholder='Choose sub category';
-    if(subcatValue){
-      if (thirdOptions) {
-        thirdcatPlaceholder = 'Select ...';
-        thirdDisabled = false;
-      } else {
-        thirdcatPlaceholder = "This subcategory doesn't have 3rd category";
-      }
-    }
-
     // Render urls according to selected options
     // For now, only area/mrt has third categories.
     let url = '';
     if(!maincatValue){
-      url = '{ Choose main category } / { Choose sub category } / { Choose third category } /';
+      url = '{ Choose main category } / { Choose sub category } /';
     } else if (!subcatValue) {
-      url = maincatValue + ' / { Choose sub category } / { Choose third category } /';
-    } else if (thirdcatValue) {
-        url = maincatValue + ' / ' + subcatValue + ' / ' + thirdcatValue + ' /'
+      url = maincatValue + ' / { Choose sub category } /';
     } else {
       url = maincatValue + ' / ' + subcatValue + ' /'
     }
 
     return (
       <div className="category-form form-group">
-        <h3>Set categories (Level is until 2 or 3? not sure)</h3>
+        <h3>Set categories</h3>
         <div className="category-selector">
           <MainCategoryForm
             selectedItem={this.state.selectedItem}
@@ -101,15 +79,6 @@ class CategoryForm extends Component {
             options={subOptions}
             placeholder={subcatPlaceholder}
             disabled={subDisabled}
-          />
-          <h4>third category(if it exsists. For now, only area/mrt has third categories.)</h4>
-          <Select
-            name="third-cat-selector"
-            value={_.get(this.state, 'thirdSelectedOption.value', '')}
-            onChange={this.onChangeThird}
-            options={thirdOptions}
-            placeholder={thirdcatPlaceholder}
-            disabled={thirdDisabled}
           />
         </div>
         <p>URL : <span>https://www.estopolis.com / article / {url} article_title</span></p>
--- src/data/third_categories.js
+++
@@ -1,8 +0,0 @@
-exports.MRT = [
-  { value: 'Asok', label: 'Asok', id: 0 },
-  { value: 'Rama9', label: 'Rama9', id: 1 },
-];
-
-exports.BTS = [
-  { value: 'Siam', label: 'Siam', id: 0 },
-];

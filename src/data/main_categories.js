2b231f861247b18d45428b5ac5e62f4842987c1b
YusukeHidaka <ysk91.akg@gmail.com>
2018-01-17 17:11:00 +0700

    change icon source to svg

--- src/data/main_categories.js
+++ src/data/main_categories.js
@@ -1,13 +1,13 @@
 module.exports = [
-	{ value: 'ลงทุนคอนโด', label: 'ลงทุนคอนโด', id: 0, icon: 'signal'},
-	{ value: 'สินเชื่อและการเงิน', label: 'สินเชื่อและการเงิน', id: 1, icon: 'money'},
-	{ value: 'เคล็ดลับการดูแลคอนโด', label: 'เคล็ดลับการดูแลคอนโด', id: 2, icon: 'cog'},
-	{ value: 'รอบรั้วคอนโด', label: 'รอบรั้วคอนโด', id: 3, icon: 'map-marker'},
-	{ value: 'คู่มืออยู่คอนโด', label: 'คู่มืออยู่คอนโด', id: 4, icon: 'lightbulb-o'},
-	{ value: 'ไลฟ์สไตล์', label: 'ไลฟ์สไตล์', id: 5, icon: 'shopping-cart'},
-	{ value: 'ตกแต่งคอนโด', label: 'ตกแต่งคอนโด', id: 6, icon: 'heart-o'},
-	{ value: 'ข่าวสารคอนโด', label: 'ข่าวสารคอนโด', id: 7, icon: 'bullhorn'},
-	{ value: 'คู่มือชาวคอนโด', label: 'คู่มือชาวคอนโด', id: 8, icon: 'hand-o-left'},
+	{ value: 'ลงทุนคอนโด', label: 'ลงทุนคอนโด', id: 0, icon: 'increased-revenue'},
+	{ value: 'สินเชื่อและการเงิน', label: 'สินเชื่อและการเงิน', id: 1, icon: 'credit-card'},
+	{ value: 'เคล็ดลับการดูแลคอนโด', label: 'เคล็ดลับการดูแลคอนโด', id: 2, icon: 'printer-tool'},
+	{ value: 'รอบรั้วคอนโด', label: 'รอบรั้วคอนโด', id: 3, icon: 'three-buildings'},
+	{ value: 'คู่มืออยู่คอนโด', label: 'คู่มืออยู่คอนโด', id: 4, icon: 'light-bulb-on'},
+	{ value: 'ไลฟ์สไตล์', label: 'ไลฟ์สไตล์', id: 5, icon: 'coffee'},
+	{ value: 'ตกแต่งคอนโด', label: 'ตกแต่งคอนโด', id: 6, icon: 'armchair-silhouette'},
+	{ value: 'ข่าวสารคอนโด', label: 'ข่าวสารคอนโด', id: 7, icon: 'news-logo'},
+	{ value: 'คู่มือชาวคอนโด', label: 'คู่มือชาวคอนโด', id: 8, icon: 'book'},
 ];
 
 // icon is named from http://fontawesome.io/icons/

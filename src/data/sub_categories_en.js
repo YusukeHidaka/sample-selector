791e1b28c2b128d0b417e1a03e44316fa3a4e15e
YusukeHidaka <ysk91.akg@gmail.com>
2018-01-17 15:51:04 +0700

    save en version

---
+++ src/data/sub_categories_en.js
@@ -0,0 +1,60 @@
+exports.Investment = [
+	{ value: 'Rent', label: 'Rent', id: 0 },
+	{ value: 'Buy', label: 'Buy', id: 1 },
+	{ value: 'Sell', label: 'Sell', id: 2 },
+];
+
+exports.Money = [
+	{ value: 'Finace', label: 'Finace', id: 0 },
+	{ value: 'Credit', label: 'Credit', id: 1 },
+];
+
+exports.Repairing = [
+	{ value: 'Reform', label: 'Reform', id: 0 },
+	{ value: 'Renovation', label: 'Renovation', id: 1 },
+];
+
+exports.Area = [
+	{ value: 'BTS', label: 'BTS', id: 0 },
+	{ value: 'MRT', label: 'MRT', id: 1 },
+	{ value: 'SurveyLocation', label: 'Survey Location', id: 2 },
+	{ value: 'AirportLink', label: 'Airport Link', id: 3 },
+];
+
+exports.LivingTips = [
+	{ value: 'Cleaning', label: 'Cleaning', id: 0 },
+	{ value: 'Cooking', label: 'Cooking', id: 1 },
+	{ value: 'Health', label: 'Health', id: 2 },
+	{ value: 'Law', label: 'Law', id: 3 },
+	{ value: 'Solving', label: 'Solving', id: 4 },
+];
+
+exports.LifeStyle = [
+	{ value: 'Shopping', label: 'Shopping', id: 0 },
+	{ value: 'Travel', label: 'Travel', id: 1 },
+	{ value: 'Innovation', label: 'Innovation', id: 2 },
+	{ value: 'Hangout', label: 'Hangout', id: 3 },
+];
+
+exports.Decoration = [
+	{ value: 'Materials', label: 'Materials', id: 0 },
+	{ value: 'Furniture', label: 'Furniture', id: 1 },
+	{ value: 'Ideas', label: 'Ideas', id: 2 },
+	{ value: 'DIY', label: 'DIY', id: 3 },
+	{ value: 'Interior', label: 'Interior', id: 4 },
+	{ value: 'FengShui', label: 'Feng Shui', id: 5 },
+];
+
+exports.News = [
+	{ value: 'Promotion', label: 'Promotion', id: 0 },
+	{ value: 'Traffic', label: 'Traffic', id: 1 },
+	{ value: 'EventCondo', label: 'Event Condo', id: 2 },
+	{ value: 'RealEstate', label: 'Real Estate', id: 3 },
+];
+
+exports.Guide = [
+	{ value: 'Sell', label: 'Sell', id: 0 },
+	{ value: 'Rent', label: 'Rent', id: 1 },
+	{ value: 'Invest', label: 'Invest', id: 2 },
+	{ value: 'Buy', label: 'Buy', id: 3 },
+];
